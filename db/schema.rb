a62220f20dabba0d24e84c0547905be83e001eb3
Stefan Schmidt <schlubbi@me.com>
2013-04-11 19:05:32 +0200

    update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130411170124) do
+
+  create_table "users", :force => true do |t|
+    t.string   "last_name"
+    t.string   "first_name"
+    t.string   "major"
+    t.string   "email"
+    t.string   "role"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
