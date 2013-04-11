835083d69efce14cb8a4f7273689ee9e0191a9e4
Stefan Schmidt <schlubbi@me.com>
2013-04-11 18:21:59 +0200

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImiMaps::Application.config.secret_token = '3d19a296d2bf8410146025df1a968d929763628d4512c2787c4a52a1f5645b16d612b3bcc1ca4d37c5699b93f3179b89c0b3b564b4f1767b102657b3a8b798b0'
