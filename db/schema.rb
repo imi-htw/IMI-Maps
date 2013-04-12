f6151021c74bf62880d39118c4a691847b9ddf7d
Bastien Grasnick <bastiengrasnick@arcor.de>
2013-04-12 11:08:56 +0200

    add working picture upload for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130411195051) do
+ActiveRecord::Schema.define(:version => 20130412090122) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20130411195051) do
     t.string   "role"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "picture"
   end
 
 end
