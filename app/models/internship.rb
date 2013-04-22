471e12e8c7f4bee6dcd7dfcdf4fcda297feeb561
Stefan Schmidt <schlubbi@me.com>
2013-04-22 09:59:36 +0200

    add programming languages to internship views

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -1,5 +1,5 @@
 class Internship < ActiveRecord::Base
-  attr_accessible :living_costs, :orientation, :salary, :working_hours
+  attr_accessible :living_costs, :orientation, :salary, :working_hours, :programming_language_ids
 
   # associations
   belongs_to :user
