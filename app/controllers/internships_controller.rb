687580b09d574841a41a0106c8f528df41e09a61
Stefan Schmidt <schlubbi@me.com>
2013-04-23 18:30:14 +0200

    refactor internship controller

--- app/controllers/internships_controller.rb
+++ app/controllers/internships_controller.rb
@@ -1,36 +1,25 @@
 class InternshipsController < ApplicationController
-  before_filter :get_programming_languages, :only => [:new, :edit]
+  respond_to :html, :json
+  before_filter :get_programming_languages, :only => [:new, :edit, :update, :create]
   # GET /internships
   # GET /internships.json
   def index
     @internships = Internship.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @internships }
-    end
+    respond_with(@internships)
   end
 
   # GET /internships/1
   # GET /internships/1.json
   def show
     @internship = Internship.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @internship }
-    end
+    respond_with(@internship)
   end
 
   # GET /internships/new
   # GET /internships/new.json
   def new
     @internship = Internship.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @internship }
-    end
+    respond_with(@internship)
   end
 
   # GET /internships/1/edit
@@ -43,15 +32,8 @@ class InternshipsController < ApplicationController
   def create
     @internship = Internship.new(params[:internship])
 
-    respond_to do |format|
-      if @internship.save
-        format.html { redirect_to @internship, notice: 'Internship was successfully created.' }
-        format.json { render json: @internship, status: :created, location: @internship }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @internship.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = "Internship was successfully created" if @internship.save
+    respond_with(@internship)
   end
 
   # PUT /internships/1
@@ -59,15 +41,10 @@ class InternshipsController < ApplicationController
   def update
     @internship = Internship.find(params[:id])
 
-    respond_to do |format|
-      if @internship.update_attributes(params[:internship])
-        format.html { redirect_to @internship, notice: 'Internship was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @internship.errors, status: :unprocessable_entity }
-      end
+    if @internship.update_attributes(params[:internship])
+      flash[:notice] = 'Internship was successfully updated.'
     end
+    respond_with(@internship)
   end
 
   # DELETE /internships/1
@@ -82,11 +59,12 @@ class InternshipsController < ApplicationController
     end
   end
 
-  private 
+  private
 
   def get_programming_languages
     @programming_languages = ProgrammingLanguage.order(:name).map do |p|
       [p.name, p.id]
     end
   end
+
 end
