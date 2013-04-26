90ff36d13acdb58699b4172e68abd42e3140bc8a
Stefan Schmidt <schlubbi@me.com>
2013-04-26 13:40:39 +0200

    add model validation and tests for internship

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -1,6 +1,14 @@
 class Internship < ActiveRecord::Base
   attr_accessible :living_costs, :orientation, :salary_id, :working_hours, :programming_language_ids, :attachments_attributes, :rating
 
+  # validations
+  validates :orientation, :presence => true
+  validates :programming_language_ids, :presence => true
+  validates :living_costs, :presence => true
+  validates :salary_id, :presence => true, :numericality => true
+  validates :rating, :presence => true, :numericality => true
+
+
   # associations
   belongs_to :user
   belongs_to :company
