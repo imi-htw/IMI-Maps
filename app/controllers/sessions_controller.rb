4c5aa2e974d15edaf14dc7b781896023fd6bf64f
Stefan Schmidt <schlubbi@me.com>
2013-05-04 14:54:24 +0200

    change root to session#new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in!"
+      redirect_to companies_url, notice: "Logged in!"
     else
       binding.pry
       flash.now.alert = "Email or password is invalid"
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ ImiMaps::Application.routes.draw do
 
     resources :sessions
 
-		root to: 'companies#index'
+		root to: 'sessions#new'
     get 'signup', to: 'users#new', as: 'signup'
     get 'login', to: 'sessions#new', as: 'login'
     get 'logout', to: 'sessions#destroy', as: 'logout'
