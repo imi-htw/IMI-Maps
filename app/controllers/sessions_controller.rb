eed9b4b4a14e16352376f23b7748186ea5f80bf9
Stefan Schmidt <schlubbi@me.com>
2013-05-04 14:37:50 +0200

    add session controller and view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
+      binding.pry
       flash.now.alert = "Email or password is invalid"
       render "new"
     end
