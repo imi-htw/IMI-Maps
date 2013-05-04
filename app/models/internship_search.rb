b6031557cf39075ebb281621d6560afc84aa0f15
Stefan Schmidt <schlubbi@me.com>
2013-05-04 18:09:34 +0200

    start find_internships function

--- app/models/internship_search.rb
+++ app/models/internship_search.rb
@@ -1,3 +1,19 @@
 class InternshipSearch < ActiveRecord::Base
   attr_accessible :city, :country, :industry, :max_salary, :min_salary, :orientation, :rating
+
+  def internships
+    @internships || = find_internships
+  end
+
+private 
+  
+  def find_internships
+    internships = Internship.includes(:company)
+    internships = internships.where('companies.country like ?', country)
+    internships = internships.where('companies.city like ?', city)
+    internships = internships.where('orientation like ?', orientation)
+    internships = internships.where('industry like ?', industry)
+    internships = internships.where(:rating => rating)
+    internships
+  end
 end
