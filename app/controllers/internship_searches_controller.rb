f7798fd663a5930bfdfb068c59c27b70365c1edc
Stefan Schmidt <schlubbi@me.com>
2013-05-04 18:09:12 +0200

    add internship_searches controller

--- app/controllers/internship_searches_controller.rb
+++ app/controllers/internship_searches_controller.rb
@@ -1,2 +1,14 @@
 class InternshipSearchesController < ApplicationController
+  def new
+    @search = Search.new
+  end
+
+  def create
+    @search = Search.create!(params[:search])
+    redirect_to @search
+  end
+
+  def show
+    @search = Search.find(params[:id])
+  end
 end
