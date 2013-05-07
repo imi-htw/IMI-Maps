55033abd1cbc7fa90e2a2c4fd00be7fd7b3f647d
Andrew Tristan <andrewtristan17@gmail.com>
2013-05-07 15:27:31 +0200

    comment

--- db/schema.rb
+++ db/schema.rb
@@ -1,98 +1,3 @@
-<<<<<<< HEAD
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
-
-ActiveRecord::Schema.define(:version => 20130504113550) do
-
-  create_table "attachments", :force => true do |t|
-    t.text     "description"
-    t.string   "file"
-    t.integer  "attachable_id"
-    t.string   "attachable_type"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
-  add_index "attachments", ["attachable_id"], :name => "index_attachments_on_attachable_id"
-
-  create_table "companies", :force => true do |t|
-    t.string   "name"
-    t.integer  "number_employees"
-    t.string   "industry"
-    t.string   "website"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-    t.float    "latitude"
-    t.float    "longitude"
-    t.string   "city"
-    t.string   "country"
-    t.string   "street"
-    t.integer  "zip"
-    t.string   "main_language"
-  end
-
-  create_table "contact_people", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "phone"
-    t.integer  "company_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "internships", :force => true do |t|
-    t.string   "orientation"
-    t.integer  "salary_id"
-    t.float    "working_hours"
-    t.float    "living_costs"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.integer  "rating"
-  end
-
-  create_table "internships_programming_languages", :id => false, :force => true do |t|
-    t.integer "programming_language_id"
-    t.integer "internship_id"
-  end
-
-  create_table "programming_languages", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "salaries", :force => true do |t|
-    t.string   "amount"
-    t.integer  "order_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "last_name"
-    t.string   "first_name"
-    t.string   "major"
-    t.string   "email"
-    t.string   "role"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "picture"
-    t.string   "password_digest"
-  end
-
-end
-=======
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -202,7 +107,7 @@ ActiveRecord::Schema.define(:version => 20130430131305) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "picture"
+    t.string   "password_digest"
   end
 
 end
->>>>>>> comment
