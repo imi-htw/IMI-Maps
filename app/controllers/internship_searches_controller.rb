fbbe26e2ecfe6793596aceb5dd8a361797d0221a
Stefan Schmidt <schlubbi@me.com>
2013-05-08 08:55:37 +0200

    add internship search

--- app/controllers/internship_searches_controller.rb
+++ app/controllers/internship_searches_controller.rb
@@ -1,14 +1,15 @@
 class InternshipSearchesController < ApplicationController
   def new
-    @search = Search.new
+    @internship_search = InternshipSearch.new
   end
 
   def create
-    @search = Search.create!(params[:search])
-    redirect_to @search
+    @internship_search = InternshipSearch.create!(params[:internship_search])
+    binding.pry
+    redirect_to @internship_search
   end
 
   def show
-    @search = Search.find(params[:id])
+    @internship_search = InternshipSearch.find(params[:id])
   end
 end
--- app/models/internship_search.rb
+++ app/models/internship_search.rb
@@ -1,19 +1,23 @@
 class InternshipSearch < ActiveRecord::Base
-  attr_accessible :city, :country, :industry, :max_salary, :min_salary, :orientation, :rating
+  attr_accessible :city, :country, :industry, :max_salary, :min_salary, :orientation, :rating, :programming_language_ids
+  attr_accessor :programming_language_ids
 
   def internships
-    @internships || = find_internships
+    @internships = find_internships
   end
 
 private 
   
   def find_internships
-    internships = Internship.includes(:company)
-    internships = internships.where('companies.country like ?', country)
-    internships = internships.where('companies.city like ?', city)
-    internships = internships.where('orientation like ?', orientation)
-    internships = internships.where('industry like ?', industry)
-    internships = internships.where(:rating => rating)
-    internships
+    internships = Internship.joins(:company).joins(:salary).joins(:programming_languages)
+    internships = internships.where("internships_programming_languages.programming_language_id IN (?)", programming_language_ids) if programming_language_ids.present?
+    internships = internships.where('companies.country like ?', country) if country.present?
+    internships = internships.where('companies.city like ?', city) if city.present?
+    internships = internships.where('orientation like ?', orientation) if orientation.present?
+    internships = internships.where('industry like ?', industry) if industry.present?
+    internships = internships.where(:rating => rating) if rating
+    internships = internships.where('salaries.min_amount >= ?', min_salary) if min_salary
+    internships = internships.where('salaries.max_amount <= ?', max_salary) if max_salary
+    internships.uniq
   end
 end
