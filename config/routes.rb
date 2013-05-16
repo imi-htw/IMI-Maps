87e56e600bb7803a95950cf87a5e14a5ecea97e8
Stefan Schmidt <schlubbi@me.com>
2013-05-16 15:45:41 +0200

    move active admin to the bottom of the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 ImiMaps::Application.routes.draw do
 
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+
 
 	scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
 		resources :internships
@@ -37,8 +36,11 @@ ImiMaps::Application.routes.draw do
 
 	match '*path', to: redirect("/#{I18n.default_locale}/%{path}") 
 	match '', to: redirect("/#{I18n.default_locale}/")
+
   match 'de', to: redirect("/de/sessions#new")
   match 'en', to: redirect("/en/sessions#new")
+    devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
