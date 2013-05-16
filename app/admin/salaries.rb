4841371bf34065b739f5d6bd0161f52f8721eed5
Bastien Grasnick <bastiengrasnick@arcor.de>
2013-05-16 14:25:56 +0200

    add all models to admin

---
+++ app/admin/salaries.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register Salary do
+  
+end
--- app/models/salary.rb
+++ app/models/salary.rb
@@ -2,4 +2,9 @@ class Salary < ActiveRecord::Base
   attr_accessible :amount, :order_id, :min_amount, :max_amount
 
   has_many :internships
+
+  def name
+    self.amount
+  end
+  
 end
