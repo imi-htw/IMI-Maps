28efdff6224b0b39c8c7902e44fb599eea346ae7
Stefan Schmidt <schlubbi@me.com>
2013-05-22 08:27:18 +0200

    add dummy companies to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,3 +33,9 @@ Salary.create(amount: "2100-2399", order_id: 7, min_amount: 2100, max_amount: 23
 Salary.create(amount: "2400-2699", order_id: 8, min_amount: 2400, max_amount: 2699)
 Salary.create(amount: "2700-2999", order_id: 9, min_amount: 2700, max_amount: 2999)
 Salary.create(amount: ">= 3000", order_id: 10, min_amount: 3000, max_amount: nil)
+
+Company.destroy_all
+#Company.create(name: "Samsung Electronics", industry: "electronic devices", website: "www.samsung.com", street: "1320-10, Seocho 2-dong", zip: "137-857", city: "Seoul", number_employees: 369000, main_language: "english")
+Company.create(name: "HTW Berlin", industry: "university", website: "www.htw-berlin.de", street: "Wilhelminenhofstraße 75A", zip: "12459", city: "Berlin", number_employees: 500, main_language: "deutsch")
+Company.create(name: "Yacto", industry: "e-commerce", website: "www.yacto.com", street: "1111 O'Farrell St", zip: "94019", city: "San Francisco", number_employees: 5, main_language: "english")
+Company.create(name: "Github", industry: "coding", website: "www.github.com", street: "548 4th St", zip: "94107", city: "San Francisco", number_employees: 169, main_language: "english")
