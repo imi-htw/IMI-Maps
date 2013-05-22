824c8c5c1a37c911c51bc290e453d13e14401e2f
Bastien Grasnick <bastiengrasnick@arcor.de>
2013-05-22 21:53:57 +0200

    fix some bugs in quicksearch and displaying websites/linking to them in companies#index

--- app/models/quicksearch.rb
+++ app/models/quicksearch.rb
@@ -6,13 +6,18 @@ class Quicksearch < ActiveRecord::Base
   end
 
    def find_AJAX(query)
-   	companies_out = []
-   	companies_out += Company.where('country like ?',query)
-   	companies_out += Company.where('city like ?',query)
-   	companies_out += Company.where('street like ?',query)
-   	companies_out += Company.where('industry like ?',query)
-   	companies_out += Company.where('name like ?',query)
-   	companies_out
+    if query.empty?      
+      Company.all
+    else 
+      companies_out = []
+      companies_out += Company.where('country like ?',query)
+      companies_out += Company.where('city like ?',query)
+      companies_out += Company.where('street like ?',query)
+      companies_out += Company.where('industry like ?',query)
+      companies_out += Company.where('name like ?',query)
+      companies_out
+    end
+
    	#internships = Internship.joins(:company).joins(:salary).joins(:programming_languages)
     #internships_out += internships.where("internships_programming_languages.programming_language_id IN (?)", search)
     #internships_out += internships.where('companies.country like ?', search)
