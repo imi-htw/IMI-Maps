e98f67c35923ac1cb2af55f4fb200fd691c71a17
Andrew Tristan <andrewtristan17@gmail.com>
2013-05-23 16:21:06 +0200

    fix bug in quiet_assets initializer. log to /dev/null on unix, otherwise to logger.txt

--- config/initializers/quiet_assets.rb
+++ config/initializers/quiet_assets.rb
@@ -1,6 +1,14 @@
 if Rails.env.development?
+
   #Rails.application.assets.logger = Logger.new('/dev/null')
-  Rails.application.assets.logger = Logger.new('/dev/null') ? File.exists?('/dev/null') : Logger.new('logger.txt')
+  logger = nil
+  if File.exists?('/dev/null')
+    logger = Logger.new('/dev/null')
+  else
+    logger = Logger.new('logger.txt')
+  end
+  Rails.application.assets.logger = logger
+
   Rails::Rack::Logger.class_eval do
     def call_with_quiet_assets(env)
       previous_level = Rails.logger.level
