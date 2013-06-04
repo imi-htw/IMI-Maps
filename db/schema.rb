d3051a1c41d10cfb99e95787e40d87390e6aa49f
Ollie <oliverdumas90@gmail.com>
2013-06-04 18:23:39 +0200

    internship design

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,8 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(:version => 20130602171318) do
-=======
-ActiveRecord::Schema.define(:version => 20130530095558) do
->>>>>>> internship design
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
