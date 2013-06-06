f6eb92b9ff9008010bb1fa22fe4752d1501f735d
Bastien Grasnick <bastiengrasnick@arcor.de>
2013-06-06 14:01:46 +0200

    update seeds.rb to include company, user and internship creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,3 +41,36 @@ Orientation.where(name: "Software", id:4).first_or_create
 Orientation.where(name: "Marketing", id:5).first_or_create
 Orientation.where(name: "Design", id:6).first_or_create
 Orientation.where(name: "Product Management", id:7).first_or_create
+
+Company.where(name: "Google", number_employees: 100, industry: "Web", website: "www.google.com", street: "Amphitheatre Pkwy", zip: "94043", city: "Mountain View", country: "United States", main_language: "Englisch", id:1).first_or_create
+Company.where(name: "Apple", number_employees: 100, industry: "Software", website: "http://www.apple.com/de/", street: "Infinite Loop", zip: "95014", city: "Cupertino", country: "United States", main_language: "Englisch", id:2).first_or_create
+Company.where(name: "Suzuki", number_employees: 100, industry: "Automobile", website: "http://www.globalsuzuki.com/", street: "Takatsuka ", zip: "432 8611", city: "Hamamatsu", country: "Japan", main_language: "Japanese", id:3).first_or_create
+Company.where(name: "Woolworth", number_employees: 100, industry: "Verkauf", website: "http://www.woolworthslimited.com.au/", street: "Woolworths Way", zip: "2153", city: "Bella Vista", country: "Australia", main_language: "Japanese", id:4).first_or_create
+Company.where(name: "Adecco", number_employees: 100, industry: "Personendienstleistungen", website: "http://www.adecco.com/en-us/Pages/default.aspx", street: "Bouverie St", zip: "EC4Y 8AX", city: "London", country: "United Kingdom", main_language: "Englisch", id:5).first_or_create
+Company.where(name: "AmBev", number_employees: 100, industry: "Essen und Trinken", website: "http://www.ambev.com.br/pt-br", street: "Rua Sao Cristovao", zip: "1221", city: "Rio de Janeiro", country: "Brazil", main_language: "Portugiesisch", id:6).first_or_create
+
+User.destroy_all
+user1 = User.create(last_name: "Grasnick", first_name: "Bastien", password: "test", email: "basti@imimaps.com", id:1)
+user2 = User.create(last_name: "Schmidt", first_name: "Stefan", password: "test", email: "stefan@imimaps.com", id:2)
+user3 = User.create(last_name: "Bell", first_name: "Lisa", password: "test", email: "lisa@imimaps.com", id:3)
+user4 = User.create(last_name: "Vogel", first_name: "Steffi", password: "test", email: "steffi@imimaps.com", id:4)
+
+Internship.destroy_all
+i = Internship.new(salary_id: 1, working_hours: 40.0, living_costs: 500.0, rating: 3, company_id: 1, user_id: user1, title: "Awesome Developer #1", recommend: true, orientation_id: 1, email_public: true, description: "test", semester: "Sommer 2013", id:1)
+i.programming_languages = ProgrammingLanguage.where(:id => [1,2,5])
+i.save
+i = Internship.new(salary_id: 3, working_hours: 40.0, living_costs: 500.0, rating: 4, company_id: 4, user_id: user2, title: "Awesome Developer #2", recommend: true, orientation_id: 2, email_public: true, description: "test", semester: "Sommer 2013", id:2)
+i.programming_languages = ProgrammingLanguage.where(:id => [3,2,7])
+i.save
+i = Internship.new(salary_id: 4, working_hours: 40.0, living_costs: 500.0, rating: 5, company_id: 3, user_id: user3, title: "Awesome Developer #3", recommend: true, orientation_id: 3, email_public: true, description: "test", semester: "Sommer 2013", id:3)
+i.programming_languages = ProgrammingLanguage.where(:id => [5,6,4])
+i.save
+i = Internship.new(salary_id: 2, working_hours: 40.0, living_costs: 500.0, rating: 3, company_id: 2, user_id: user4, title: "Awesome Developer #4", recommend: true, orientation_id: 2, email_public: true, description: "test", semester: "Sommer 2013", id:4)
+i.programming_languages = ProgrammingLanguage.where(:id => [2,4,6])
+i.save
+i = Internship.new(salary_id: 5, working_hours: 40.0, living_costs: 500.0, rating: 6, company_id: 6, user_id: user1, title: "Awesome Developer #5", recommend: false, orientation_id: 3, email_public: true, description: "test", semester: "Sommer 2013", id:4)
+i.programming_languages = ProgrammingLanguage.where(:id => [1,4,7])
+i.save
+i = Internship.new(salary_id: 2, working_hours: 40.0, living_costs: 500.0, rating: 6, company_id: 5, user_id: user2, title: "Awesome Developer #6", recommend: false, orientation_id: 4, email_public: true, description: "test", semester: "Sommer 2013", id:4)
+i.programming_languages = ProgrammingLanguage.where(:id => [3,6,7])
+i.save
