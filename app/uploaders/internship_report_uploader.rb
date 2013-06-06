31be98d3b604b954eddffb643a6804e601e3858b
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2013-06-06 07:50:45 +0200

    rename internship report uploader

--- app/uploaders/internship_report_uploader.rb
+++ app/uploaders/internship_report_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class InternshipReportUploaderUploader < CarrierWave::Uploader::Base
+class InternshipReportUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
