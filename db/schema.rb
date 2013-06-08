a73173dee6dc458cfc3ac241c7d1727d14157983
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2013-06-08 14:10:46 +0200

    Add new page for companies' comparison

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130608114240) do
+ActiveRecord::Schema.define(:version => 20130608120403) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -80,6 +80,11 @@ ActiveRecord::Schema.define(:version => 20130608114240) do
     t.string   "street"
     t.string   "zip"
     t.string   "main_language"
+  end
+
+  create_table "companies_compares", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "contact_people", :force => true do |t|
---
+++ spec/controllers/companies_compare_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe CompaniesCompareController do
+
+end
