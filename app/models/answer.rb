5825b05f0e1f21303b6f53d043a70ee783d37f97
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2013-06-09 10:55:37 +0200

    change validation of answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,11 @@
 class Answer < ActiveRecord::Base
   attr_accessible :body, :internship_id, :user_comment_id
 
+  validates :body, :presence => true
+  validates :internship_id, :presence => true
+  validates :user_comment_id, :presence => true
+
   belongs_to :user_comment
   belongs_to :user
 
-   validates :body, :presence => true
-   validates :internship_id, :presence => true
-   validates :user_comment_id, :presence => true
 end
