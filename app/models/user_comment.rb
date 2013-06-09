b652e5b68fc0e5e8a656d0297f6813e672ffdde0
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2013-06-09 11:25:50 +0200

    validate user_comment model

--- app/models/user_comment.rb
+++ app/models/user_comment.rb
@@ -1,5 +1,9 @@
 class UserComment < ActiveRecord::Base
   attr_accessible :body, :internship_id
+
+  validates :body, :presence => true
+  validates :internship_id, :presence => true
+
   
   belongs_to :internship
   belongs_to :user
