a7b43dde3f721d2c1c8edf9b37968fae7f07648f
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2013-06-09 11:25:04 +0200

    validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,17 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :first_name, :last_name, :major, :role, :picture, :password, :password_confirmation, :internship_authorization
   
+  validates :email, :presence => true
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :major, :presence => true
+  validates :role, :presence => true
+  validates :picture, :presence => true
+  validates :password, :presence => true
+  validates :password_confirmation, :presence => true
+  validates :internship_authorization, :presence => true
+
+  
   # associations
   has_one :internship
   has_many :user_comments, :dependent => :destroy
