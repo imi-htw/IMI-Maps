91142d98107269a0f6d47c9da82c26753e0973c6
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2013-06-09 10:53:00 +0200

    validate admin_user model

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -8,4 +8,9 @@ class AdminUser < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  validates :email, :presence => true
+  validates :password, :presence => true
+  validates :password_confirmation, :presence => true
+  validates :remember_me, :presence => true
 end
