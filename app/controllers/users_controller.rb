a5c6d1976e1dc04d0de3e056450c189380ec68eb
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2013-06-13 14:03:39 +0200

    update sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, notice: t('sign_up')
+      redirect_to overview_index_path, notice: t('sign_up')
     else
       render 'new'
     end
