35491f8af9f0e6428a3a4026f901d1cd6a431f67
Lisa Bell <lisabell.li@gmail.com>
2013-06-17 19:20:19 +0200

    next try

--- spec/requests/favorite_spec.rb
+++ spec/requests/favorite_spec.rb
@@ -1,13 +1,8 @@
 require 'spec_helper'
 
 describe Favorite do
-  before :each do
-    @fav = Favorite.new 1, 1
+   it "takes two parameters and returns a Favorite object" do
+     @fav = Favorite.new 1, 1
+     @fav.should be_an_instance_of Favorite
    end
-
-  describe "#new" do
-    it "takes two parameters and returns a Favorite object" do
-        @fav.should be_an_instance_of Favorite
-   end
-  end
 end
