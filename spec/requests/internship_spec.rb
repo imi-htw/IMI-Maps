f27e058492702d3217e05d127b45bbfea63c7ec5
Lisa Bell <lisabell.li@gmail.com>
2013-06-17 20:26:35 +0200

    Rating test auskommentiert

--- spec/requests/internship_spec.rb
+++ spec/requests/internship_spec.rb
@@ -7,12 +7,12 @@ describe Internship do
      it {should belong_to(:company)}
      it {should belong_to(:orientation)}
      it {should belong_to(:semester)}
-     it {should belong_to(:internship_rating)}
+     #it {should belong_to(:internship_rating)}
    end
 
   context 'validation' do
      it { should validate_presence_of(:user_id) }
      it { should validate_presence_of(:semester_id) }
-     it { should validate_presence_of(:rating) }
+     #it { should validate_presence_of(:rating) }
    end
 end
