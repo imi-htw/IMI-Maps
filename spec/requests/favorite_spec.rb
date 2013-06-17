4223cfd4abf13a8584a31767ece0548bdae8980a
Lisa Bell <lisabell.li@gmail.com>
2013-06-17 18:58:32 +0200

    ein doodeldi do vergessen

--- spec/requests/favorite_spec.rb
+++ spec/requests/favorite_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
-describe Favorite
-
- before :each do
+describe Favorite do
+  before :each do
     @fav = Favorite.new 1, 1
- end
+   end
 
- describe "#new" do
+  describe "#new" do
     it "takes two parameters and returns a Favorite object" do
         @fav.should be_an_instance_of Favorite
+   end
   end
- end
 end
