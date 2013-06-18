c26fde26803851339e8825aff6530d734002435d
Andrew Tristan <andrewtristan17@gmail.com>
2013-06-18 18:32:48 +0200

    add something in confirmation

--- app/controllers/favorite_compare_controller.rb
+++ app/controllers/favorite_compare_controller.rb
@@ -3,6 +3,11 @@ class FavoriteCompareController < ApplicationController
   def index
     @favorite = Favorite.find(params[:id])
     @internship = @favorite.internship
+    if(Favorite_compare.favorite_compare = true) 
+      @favorite.find(params[:id])
+
+    end
+
 
   end
 
