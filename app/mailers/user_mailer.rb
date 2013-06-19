c6391c2d3d1513a5031153f460e67a71f5705ffe
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2013-06-19 12:04:01 +0200

    email for all notifications

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,9 @@ class UserMailer < ActionMailer::Base
   	mail(:to => "#{user.first_name} #{user.last_name} <#{user.email}>", :subject => "(IMI-Maps) New Internship is created")
   end
 
+  def notification_to_email(user)
+  	@user = user
+  	mail(:to => "#{user.first_name} #{user.last_name} <#{user.email}>", :subject => "(IMI-Maps) New Notification")
+  end
+
 end
