54056232a27a5b5d37c2a3474ef3e8d132026f4e
Bastien Grasnick <bastiengrasnick@arcor.de>
2013-06-22 23:58:53 +0200

    small bug fixes and performance improvements

--- app/controllers/quicksearches_controller.rb
+++ app/controllers/quicksearches_controller.rb
@@ -11,13 +11,14 @@ class QuicksearchesController < ApplicationController
     @language_ary = []
 
     if params[:orientation].nil? and params[:semester].nil? and params[:programming_language_ids].nil? and params[:country].nil?
-      @internships = Internship.order("created_at DESC") 
+      @internships = Internship.find(:all, :include => [:company, :semester, :orientation, :programming_languages]).sort_by do |x| x.created_at end
     else
       @internships = @quicksearch.internships(params)
     end
 
     @companies = @internships.collect do |x| x.company end
-    @orientations_ary = @internships.collect do |x| x.orientation.name end
+
+    @orientations_ary = @internships.collect do |x| x.orientation end
 
     @internships.each do |i|
       i.programming_languages.each do |p|
@@ -35,45 +36,41 @@ class QuicksearchesController < ApplicationController
 
     end
 
-    ids = @internships.collect do |x| x.id end
-
     @semesters = @internships.collect do |x| x.semester end.map do |s|[s.semester,s.id] end
 
-    @programming_languages = language_ary.uniq.map do |p|[p.name, p.id] end
+    @programming_languages = @language_ary.uniq.map do |p|[p.name, p.id] end
 
     @internships_size = @internships.size
 
     @bool = Internship.all.size == @internships_size
 
     @countries = (@companies.collect do |x| x.country end)
+  
+    @orientations = @orientations_ary.uniq.map do |o| [o.name, o.id] end
 
-    @orientations = orientations_ary.uniq
-
-    countries_uniq = @countries.uniq
     ary = Array.new
-    countries_uniq.each do |x|
+    @countries.uniq.each do |x|
       ary << {:name=>x, :count=>@countries.count(x)}
     end
     @data_country = ary
 
-    language_uniq = @language_ary.uniq
     ary = Array.new
-    language_uniq.each do |x|
-      ary << {:name=>x, :count=>(@language_ary.count(x).to_f/@internships_size*100).to_i}
+    @language_ary.uniq.each do |x|
+      ary << {:name=>x.name, :count=>(@language_ary.count(x).to_f/@internships_size*100).to_i}
     end
     @data_language = ary
 
-    orientation_uniq = @orientations_ary.uniq
     ary = Array.new
-    orientation_uniq.each do |x|
-      ary << {:name=>x, :count=>@orientations_ary.count(x)}
+    @orientations_ary.uniq.each do |x|
+      ary << {:name=>x.name, :count=>@orientations_ary.count(x)}
     end
     @data_orientation = ary
 
     @countries = @countries.uniq
+    @semesters = @semesters.uniq
 
     respond_to do |format|
-      format.js { render :layout=>false, :locals => { :pins  => @pins, :internships => @internships, :paginating => @paginating } }
+      format.js { render :layout=>false, :locals => { :pins  => @pins, :internships => @internships } }
     end
   end
 
