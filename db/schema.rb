946a2933ccf10aaf4fd390949a964ba0f873f233
Stefan Schmidt <schlubbi@me.com>
2013-06-28 21:07:03 +0200

    add import_id to student model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130626224425) do
+ActiveRecord::Schema.define(:version => 20130628185922) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -300,6 +300,7 @@ ActiveRecord::Schema.define(:version => 20130626224425) do
     t.string   "email"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.integer  "import_id"
   end
 
   create_table "user_comments", :force => true do |t|
