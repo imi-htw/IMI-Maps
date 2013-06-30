b8a01760d1618328f602f51d6c20f9dc8060ec00
Stefan Schmidt <schlubbi@me.com>
2013-06-30 17:19:06 +0200

    change migration

--- db/migrate/20130630121738_change_reading_prof_in_internship_record.rb
+++ db/migrate/20130630121738_change_reading_prof_in_internship_record.rb
@@ -1,6 +1,6 @@
 class ChangeReadingProfInInternshipRecord < ActiveRecord::Migration
   def change
-    rename_column :internship_records, :report_reading_prof, :reading_prof_id
-    change_column :internship_records, :reading_prof_id, :integer  
+    remove_column :internship_records, :report_reading_prof
+    add_column :internship_records, :reading_prof_id, :integer  
   end
 end
