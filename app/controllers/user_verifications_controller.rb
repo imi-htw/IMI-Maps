569b1c1631d8fe9869388a2311db54b2729cfdda
Stefan Schmidt <schlubbi@me.com>
2013-06-30 16:23:14 +0200

    remove binding.pry :(

--- app/controllers/user_verifications_controller.rb
+++ app/controllers/user_verifications_controller.rb
@@ -6,7 +6,6 @@ class UserVerificationsController < ApplicationController
   def create
     ldap = LdapAuthentication.new("141.45.146.101", 389, params[:user_name], params[:password])
     if ldap.authorized?
-      binding.pry
       session[:enrolment_number] = params[:user_name].split("s0")[1]
       redirect_to new_user_path
     else
