0bc044ede5b82013d5f64de6d05e0fb925376631
Stefan Schmidt <schlubbi@me.com>
2013-06-30 13:28:43 +0200

    remove unused user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  attr_accessible :email, :first_name, :last_name, :major, :role, :picture, :password, :password_confirmation, :internship_authorization, :publicmail, :mailnotif, :student_id
+  attr_accessible :email, :password, :password_confirmation, :internship_authorization, :publicmail, :mailnotif, :student_id
   
   validates :email, :presence => true
-  validates :first_name, :presence => true
-  validates :last_name, :presence => true
-  #validates :major, :presence => true
-  #validates :role, :presence => true
-  #validates :picture, :presence => true
-    validates :password, :presence => true,
-                       :length => { :minimum => 2}
-  #validates :password_confirmation, :presence => true
+  validates :password, :presence => true
   validates :internship_authorization, :presence => true
 
   
@@ -22,10 +15,9 @@ class User < ActiveRecord::Base
   has_many :favorites, :dependent => :destroy
   has_many :notifications, :dependent => :destroy
 
-  mount_uploader :picture, PictureUploader
 
 
   def name
-    "#{first_name} #{last_name}"
+    "#{student.first_name} #{student.last_name}"
   end
 end
