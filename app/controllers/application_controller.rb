0c16d1bc5208c2e057a967d5d0e29f597a861e1d
Bastien Grasnick <bastiengrasnick@arcor.de>
2013-06-30 20:18:58 +0200

    fix bug when user is logged in but does not exist anymoer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,8 @@ private
 
     def current_user
       @current_user = User.find(session[:user_id]) if session[:user_id]
+      rescue ActiveRecord::RecordNotFound
+        session.destroy
     end
 
     helper_method :current_user
