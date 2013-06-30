5e1f3d8607370f559f35122a3ffb3ad13a1ad484
Stefan Schmidt <schlubbi@me.com>
2013-06-30 13:35:09 +0200

    remove contact people from the application

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130630110848) do
+ActiveRecord::Schema.define(:version => 20130630113210) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -91,16 +91,6 @@ ActiveRecord::Schema.define(:version => 20130630110848) do
     t.string   "fax"
     t.boolean  "blacklisted",      :default => false
     t.integer  "import_id"
-  end
-
-  create_table "contact_people", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "phone"
-    t.integer  "company_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
   create_table "contract_states", :force => true do |t|
