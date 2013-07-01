fe50fef5984c4927cb476f4953274e66b7ef401f
Bastien Grasnick <bastiengrasnick@arcor.de>
2013-07-01 10:31:41 +0200

    add new functionality to admin area

--- app/admin/internships.rb
+++ app/admin/internships.rb
@@ -1,3 +1,25 @@
 ActiveAdmin.register Internship do
 	filter :student_enrolment_number, :as => :select, :collection => proc { Student.all.map(&:enrolment_number).uniq }, :label => "Matrikel", :input_html => { :class => 'chosen' }
+
+	index do
+
+    column :id
+    column :title
+    column :company_name do |x|
+    	x.company.name
+    end 
+    column :student_enrolment_number do |x|
+    	x.student.enrolment_number
+    end
+    column :working_hours
+    column :living_costs
+	  column :salary
+	  column :tasks
+	  column :orientation do |x|
+	  	x.orientation.name
+	  end
+    default_actions
+
+  end
+
 end
