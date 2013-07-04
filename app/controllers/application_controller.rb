fdcbaf3782a11b2f71d5ffd44559a793fdead7ec
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2013-07-04 13:47:59 +0200

    update session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,8 @@ private
     end
 
     def current_user
-      #@current_user = User.find(session[:user_id]) if session[:user_id]
-      @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
+      @current_user = User.find(session[:user_id]) if session[:user_id]
+      #@current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
       rescue ActiveRecord::RecordNotFound
         session.destroy
         nil
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -51,14 +51,15 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     #
     if user && user.authenticate(params[:password])
-    #  session[:user_id] = user.id
-    #  redirect_to overview_index_url, notice: "Logged in!"
-      if params[:remember_me]
-        cookies.permanent[:auth_token] = user.auth_token
-      else
-        cookies[:auth_token] = user.auth_token
-      end
-      redirect_to overview_index_url, :notice => "Logged in!"
+      session[:user_id] = user.id
+      redirect_to overview_index_url, notice: "Logged in!"
+      #if params[:remember_me]
+      #  cookies.permanent[:auth_token] = user.auth_token
+      #else
+      #  cookies[:auth_token] = user.auth_token
+      #end
+      #redirect_to overview_index_url, :notice => "Logged in!"
+      
     else
       #flash.now.alert = "Email or password is invalid"
       flash[:alert] = "Email or password is invalid"
@@ -68,8 +69,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    #session[:user_id] = nil
-    cookies.delete(:auth_token)
+    session[:user_id] = nil
+    #cookies.delete(:auth_token)
     redirect_to root_url, notice: "Logged out!"
   end
   
