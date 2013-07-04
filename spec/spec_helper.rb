c0c0d0985c5a9c2960f34a76d4e9678117922065
Lisa Bell <lisabell.li@gmail.com>
2013-07-04 14:43:18 +0200

    content page and spec changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
-require File.dirname(__FILE__) + "/factories"
+#require File.dirname(__FILE__) + "/factories"
 require 'rspec/rails'
 require 'factory_girl'
 require 'rspec/autorun'
--- spec/views/homepage_spec.rb
+++ spec/views/homepage_spec.rb
@@ -2,26 +2,17 @@ require 'spec_helper'
 require 'factory_girl'
 
 
-FactoryGirl.define do
+ FactoryGirl.define do
   factory :user do
     password "test"
     email  "foo@imimaps.com"
    end
   end
 
-describe "Homepage" do
-  it "should ask for sign in" do
-    visit root_path
-    page.should have_content("E-mail")
-    page.should have_content("E-mail")
-  end
-end
-
-
 describe "Testing LogIn1" do
-      it "login data not valid because password too short" do
-      	user4 =  FactoryGirl.build(:user)                                   
-      	visit root_path
+      it "login data not valid because password too short. Render Session" do	
+      	user4 =  FactoryGirl.build(:user)
+        visit root_path
       	fill_in "email",  :with => user4.email
         fill_in "password",  :with => user4.password
         page.find('.signin-icon').click
@@ -34,11 +25,8 @@ describe "Testing LogIn1" do
       it "user is invalid when password less than 5 characters" do
        expect {user7 = User.create!(password: "tes", email: "test@imimaps.com")}.to raise_error 
       end
-   end 
-
-
-describe "Testing Login2" do
-      it "should allow user to login" do       
+   
+      it "should allow user to login with valid data and redirect to overview" do       
         @user6 = User.create!( password: "testIT", email: "test@imimaps.com")
         @user6.save
         visit root_path
@@ -52,11 +40,8 @@ describe "Testing Login2" do
         visit overview_index_path("de")
         current_path.should == "/de/overview"
         page.should have_content("Rating")
-   end
-   
-end
+      end
 
-describe "Testing Login3" do
       it "should not allow user with fake login data to login" do       
         visit root_path
         fill_in "email",  :with => "test@imimaps.com"
@@ -65,20 +50,11 @@ describe "Testing Login3" do
         visit overview_index_path("de")
         response.should render_template(:session) 
    end
-end
 
-describe "Testing Login4" do
       it "should not allow access if not logged in" do       
         visit overview_index_path("de")      
         current_path.should == "/"
-        
-       #response.should be_successful
-        #should respond_with(:redirect)
-        #current_path.should  redirect_to("overview#index")
-        #response.should render_template(:overview)
-        #uri = URI.parse(current_url)
-        #{}"#{uri.path}?#{uri.query}".should == overview_index_path(:locale => 'en')
-        #current_path.should == "/en/overview/"
-        
+        visit overview_index_path("de")      
+        current_path.should == "/"   
    end
 end
