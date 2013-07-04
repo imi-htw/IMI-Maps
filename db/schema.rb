ae4cb48f2bf5d943cdbd59e7c0a750c1ac47b459
Stefanie <mailto.stefanie@gmail.com>
2013-07-04 17:19:39 +0200

    headline color internship font

--- db/schema.rb
+++ db/schema.rb
@@ -155,7 +155,6 @@ ActiveRecord::Schema.define(:version => 20130704133212) do
     t.string   "supervisor_name"
     t.string   "supervisor_email"
     t.string   "comment"
-    t.integer  "reading_prof_id",                          :limit => 255
     t.date     "certificate_to_prof"
     t.date     "certificate_signed_by_prof"
     t.date     "certificate_signed_by_internship_officer"
@@ -166,8 +165,9 @@ ActiveRecord::Schema.define(:version => 20130704133212) do
     t.integer  "contract_state_id"
     t.integer  "report_state_id"
     t.integer  "certificate_state_id"
-    t.datetime "created_at",                                              :null => false
-    t.datetime "updated_at",                                              :null => false
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
+    t.integer  "reading_prof_id"
   end
 
   create_table "internship_searches", :force => true do |t|
@@ -324,6 +324,9 @@ ActiveRecord::Schema.define(:version => 20130704133212) do
     t.boolean  "publicmail"
     t.boolean  "mailnotif"
     t.integer  "student_id"
+    t.string   "auth_token"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end
