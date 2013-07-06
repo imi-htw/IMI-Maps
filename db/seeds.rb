ef0877c203a2c1c64ee148fdcdfe2f8c1c7774f3
Lisa Bell <lisabell.li@gmail.com>
2013-07-06 15:48:25 +0200

    fix non clickable chart. seeds neuladen dafür

--- db/seeds.rb
+++ db/seeds.rb
@@ -60,10 +60,10 @@ Orientation.where(name: "Software", id:4).first_or_create
 Orientation.where(name: "Marketing", id:5).first_or_create
 Orientation.where(name: "Design", id:6).first_or_create
 Orientation.where(name: "Product Management", id:7).first_or_create
-Orientation.where(name: "Graphics & 3D", id: 8).first_or_create
+Orientation.where(name: "Graphics/3D", id: 8).first_or_create
 Orientation.where(name: "Video", id: 9).first_or_create
 Orientation.where(name: "Concept", id: 10).first_or_create
-Orientation.where(name: "Administration & Support", id: 11).first_or_create
+Orientation.where(name: "Administration/Support", id: 11).first_or_create
 
 
 
