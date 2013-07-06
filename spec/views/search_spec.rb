68def21fdeeb0f96fb9a834b565a362b4d3a6060
Lisa Bell <lisabell.li@gmail.com>
2013-07-06 14:43:57 +0200

    specs changes

--- spec/views/search_spec.rb
+++ spec/views/search_spec.rb
@@ -9,7 +9,7 @@ describe "Detailed Search Page should have content" do
         fill_in "email",  :with => @user6.email
         fill_in "password",  :with => @user6.password
         page.find('.signin-icon').click
-        visit search_index_path("de") 
+        visit "/de/internships"
         page.should have_content("Lebenshaltungskosten")
         page.should have_content("Bewertung")
         page.should have_content("Gehalt")
@@ -17,5 +17,13 @@ describe "Detailed Search Page should have content" do
         page.should have_content("Cost of living")
         page.should have_content("salary")
         page.should have_content("Rating")
+        page.should have_content(I18n.t('search.rating'))
+        page.should have_content(I18n.t('search.resultnr'))
+        #select('SS 13', :from => 'semester')
+        #select('Brazil', :from => 'Choose a country')
+        #select('United Kingdom', :from => 'country')
+        #select('SS 12', :from => 'Choose a semester')
+        #click_button "submit"
+        #click_button "reset_chosen"
      end
 end
