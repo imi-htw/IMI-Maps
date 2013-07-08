3ab35a7bfde0ed37ca8f1a9dd418e1572b8c3817
Stefan Schmidt <schlubbi@me.com>
2013-07-08 19:13:28 +0200

    change active admin interface

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register User do
 
+  form do |f|
+    f.inputs "User" do
+    f.input :student_id, as: :select, collection: Student.order(:enrolment_number).collect { |s| ["#{s.enrolment_number} #{s.last_name} #{s.first_name}", s.id] }
+
+    f.input :publicmail
+    f.input :mailnotif
+    f.input :email
+    f.input :password
+    f.input :password_confirmation
+    end
+    f.buttons
+
+  end
 end
