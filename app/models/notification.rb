5bcd1b4fe5078c0cc0406fc1a2d4283d3a4b9ed1
Stefan Schmidt <schlubbi@me.com>
2013-07-09 11:54:20 +0200

    fix notification menu item

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,9 +1,7 @@
 class Notification < ActiveRecord::Base
   attr_accessible :read, :text, :link
 
-  validates :read, :presence => true
   validates :text, :presence => true
-  validates :link, :presence => true
 
   belongs_to :user
 end
