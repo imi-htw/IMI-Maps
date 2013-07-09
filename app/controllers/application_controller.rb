776a55f8eceacfa665c6e53dff1be857a136f82e
Stefan Schmidt <schlubbi@me.com>
2013-07-09 10:22:01 +0200

    revert locale settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ private
     end
 
     def default_url_options(options = {})
-      { :locale => I18n.locale == I18n.default_locale ? nil : I18n.locale  }
+      { locale: I18n.locale }
     end
 
     def current_user
