baa537b9ebca19f3e15de1b4b4fd87c902b17424
Lisa Bell <lisabell.li@gmail.com>
2013-07-09 03:07:05 +0200

    small test additions and spelling mistakes

--- spec/views/homepage_spec.rb
+++ spec/views/homepage_spec.rb
@@ -59,12 +59,19 @@ describe "Testing LogIn1" do
         current_path.should == "/"   
    end
 
-       it "should not allow access if not logged in" do       
+       it "should not allow access to any page if not logged in" do       
         visit general_index_path("de")      
         current_path.should == "/"
         visit financing_index_path("de")      
         current_path.should == "/"
         visit download_index_path("de")      
         current_path.should == "/"
+        visit faq_index_path("de")      
+        current_path.should == "/"
+        visit "/de/internships"     
+        current_path.should == "/"
+        visit overview_index_path("de")      
+        current_path.should == "/"
+          
    end
 end
