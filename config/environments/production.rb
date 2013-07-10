85daef2a322c0dcad7386b4f8b679c060e7e9f33
Stefan Schmidt <schlubbi@me.com>
2013-07-10 13:58:00 +0200

    set default_url_options for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,7 @@ ImiMaps::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "imi-map.f4.htw-berlin.de" }
   config.action_mailer.smtp_settings = {
     host: "smtp.gmail.com",
     port: 587,
