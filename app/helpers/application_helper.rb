2ff981c71342986a91dd66769f81fc5e79af5c27
Ollie <oliverdumas90@gmail.com>
2013-07-10 19:47:49 +0200

    small bug fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
         when :notice then "alert alert-info"
         when :success then "alert alert-success"
         when :error then "alert alert-error"
-        when :alert then "alert alert-error"
+        when :alert then "alert alert-info"
     end
 	end
 
