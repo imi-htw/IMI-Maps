f8ff201a3607195b5b15ae7f5980c85479fc92d9
Stefan Schmidt <schlubbi@me.com>
2013-07-10 12:10:13 +0200

    prepare deploy

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -3,19 +3,19 @@ class Internship < ActiveRecord::Base
   attr_accessible :living_costs, :orientation_id, :salary, :working_hours, :programming_language_ids, :internship_rating_id,
     :company_id, :user_id, :title, :recommend, :email_public, :semester_id, :description, :internship_report, :student_id, :start_date, :end_date, :operational_area, :tasks, :internship_state_id, :reading_prof_id, :payment_state_id, :registration_state_id, :contract_state_id, :report_state_id, :certificate_state_id, :certificate_signed_by_internship_officer, :certificate_signed_by_prof, :certificate_to_prof, :comment, :supervisor_email, :supervisor_name, :internship_rating_attributes, :completed
 
-  validates :company_id, :presence => true
-  validates :semester_id, :presence => true
-  validates :student_id, :presence => true
-  validates :registration_state_id, :presence => true
-  validates :payment_state_id, :presence => true
-  validates :contract_state_id, :presence => true
-  validates :report_state_id, :presence => true
-  validates :certificate_state_id, :presence => true
-  validates :reading_prof_id, :presence => true
-  validates :certificate_to_prof, :presence => true
-  validates :certificate_signed_by_prof, :presence => true
-  validates :certificate_signed_by_internship_officer, :presence => true
-  validates :internship_state, :presence => true
+#  validates :company_id, :presence => true
+#  validates :semester_id, :presence => true
+#  validates :student_id, :presence => true
+#  validates :registration_state_id, :presence => true
+#  validates :payment_state_id, :presence => true
+#  validates :contract_state_id, :presence => true
+#  validates :report_state_id, :presence => true
+#  validates :certificate_state_id, :presence => true
+#  validates :reading_prof_id, :presence => true
+#  validates :certificate_to_prof, :presence => true
+#  validates :certificate_signed_by_prof, :presence => true
+#  validates :certificate_signed_by_internship_officer, :presence => true
+#  validates :internship_state, :presence => true
      
 
   belongs_to :user
--- app/observers/internship_observer.rb
+++ app/observers/internship_observer.rb
@@ -2,14 +2,14 @@ class InternshipObserver < ActiveRecord::Observer
 
   def after_create(model)
     student = Student.find(model.student_id)
-    unless student.present? && student.has_user? 
-      UserMailer.invite_student(student).deliver
+    unless student.present? && student.has_user?
+      #UserMailer.invite_student(student).deliver
     end
   end
 
   def after_update(model)
     if model.report_state_id_changed? and model.editable?
-      InternshipNotificationHandler.new(internship: model).notify
+      #InternshipNotificationHandler.new(internship: model).notify
     end
   end
 end
