f3648067e9435821a9ba7d18923fbd313f785680
Stefan Schmidt <schlubbi@me.com>
2013-07-12 09:48:40 +0200

    remove pry call from user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,8 +33,6 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    binding.pry
-
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to @user, notice: 'Details were successfully updated.' }
