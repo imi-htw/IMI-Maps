bcf10352602c65fe3e1596297e1a12eb601421eb
Stefan Schmidt <schlubbi@me.com>
2013-07-30 17:54:24 +0200

    remove setup_mail initializer, since it's configured in the production environment

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-	:address				=> "smtp.gmail.com",
-	:port					=> 587,
-	:domain					=> "gmail.com",
-	:user_name				=> "IMI.Prakti.Info",
-	:password				=> "imimaps7",
-	:authentication			=> "plain",
-	:enable_starttls_auto	=> true
-}
