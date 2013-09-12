90354874c648061fa866ee10cb8efda792c3743f
Stefan Schmidt <schlubbi@me.com>
2013-09-12 15:11:58 +0200

    remove title validation from internship model

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -3,7 +3,7 @@ class Internship < ActiveRecord::Base
   attr_accessible :attachments_attributes, :living_costs, :orientation_id, :salary, :working_hours, :programming_language_ids, :internship_rating_id,
     :company_id, :user_id, :title, :recommend, :email_public, :semester_id, :description, :internship_report, :student_id, :start_date, :end_date, :operational_area, :tasks, :internship_state_id, :reading_prof_id, :payment_state_id, :registration_state_id, :contract_state_id, :report_state_id, :certificate_state_id, :certificate_signed_by_internship_officer, :certificate_signed_by_prof, :certificate_to_prof, :comment, :supervisor_email, :supervisor_name, :internship_rating_attributes, :completed
 
-  validates :title, :semester_id, :student_id, presence: true
+  validates :semester_id, :student_id, presence: true
 
   belongs_to :user
   belongs_to :company
