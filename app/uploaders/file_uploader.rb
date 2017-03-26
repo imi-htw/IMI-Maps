7465a2e2925a01a571289008cb751ca80043b5c4
Simon Albrecht <simon.albrecht@dev-sector.net>
2017-03-26 23:31:37 +0200

    adding env scope to uploaders

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -12,7 +12,7 @@ class FileUploader < CarrierWave::Uploader::Base
 
   process :resize_to_fit => [1024, 768]
 
-  version :thumb do 
+  version :thumb do
     process :resize_to_limit => [200, 200]
   end
 
@@ -23,8 +23,9 @@ class FileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
